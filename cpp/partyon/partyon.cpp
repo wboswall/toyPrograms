fe89afe4f13354619ecaacb78f3282233db0a7e4
William Boswall <wboswall@live.com>
2022-05-18 06:08:16 -0300

    Commit PartyOn
    fixed minor grammar errors.

--- cpp/partyon/partyon.cpp
+++ cpp/partyon/partyon.cpp
@@ -18,19 +18,19 @@ void calcTotal(){
 	double totalCupCost = 0.0;
 	double platesPurchased = 0.0;
 	double cupsPurchased = 0.0;
-	cout << "Enter the number of cups " << endl;
+	cout << "Enter the number of cups: " << endl;
 	cin  >> numCups;
-	cout << "Enter the number of plates " << endl;
+	cout << "Enter the number of plates: " << endl;
 	cin >> numPlates;
-	cout << "Enter the plate price " << endl;
+	cout << "Enter the plate price: " << endl;
 	cin >> platePrice;
-	cout << "Enter the cup price " << endl;
+	cout << "Enter the cup price: " << endl;
 	cin >> cupPrice;
-	cout << "Enter the sales tax in decimal form for your province/state. For example 15% would be .15 " << endl;
+	cout << "Enter the sales tax in decimal form for your province/state. For example 15% would be .15: " << endl;
 	cin >> salesTax;
 	totalPlateCost = platesPurchased * platePrice;
 	totalCupCost = cupsPurchased * cupPrice;
 	subTotal = totalCupCost + totalPlateCost;
 	total = (subTotal * salesTax) + subTotal;
-	cout << "The total cost is: " << total << "Thank you for shopping at Party On Supplies.  We appreciate your business" << endl;
+	cout << "The total cost is: " << total << "Thank you for shopping at Party On Supplies.  We appreciate your business." << endl;
 }
