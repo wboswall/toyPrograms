ddd622015f83485f0ffedaac23ea65d475d29d9d
William Boswall <wboswall@live.com>
2022-05-18 06:06:20 -0300

    Create PartyOn
    Initial commit of program

--- cpp/partyon/partyon.cpp
+++ cpp/partyon/partyon.cpp
@@ -1,40 +1,36 @@
-/*	Created on: 31 Nov 2016
-	Revised Date:
-	Project Name: Party On Co.
-    Description: Calculates and displays the total cost.
-	Coded by: WB
-*/
 #include <iostream>
 using namespace std;
-int main()
-{
-    //declare variables
-    double cupPrice=0.0;
-    double platePrice=0.0;
-    double cupsPurchased=0.0;
-    double platesPurchased=0.0;
-    double salesTaxRate=0.0;
-    double subTotal=0.0;
-    double totalCupsCost=0.0;
-    double totalPlatesCost=0.0;
-    double totalCost=0.0;
-    //get input
-    cout<<"cup price: "<<endl;
-    cin>>cupPrice;
-    cout<<"plate price: "<<endl;
-    cin>>platePrice;
-    cout<<"cups purchased: "<<endl;
-    cin>>cupsPurchased;
-    cout<<"plates purchased: "<<endl;
-    cin>>platesPurchased;
-    cout<<"sales tax rate: "<<endl;
-    cin>>salesTaxRate;
-    //calculate totalCupCost and TotalPlatesCost
-    totalCupsCost=cupsPurchased*cupPrice;
-    totalPlatesCost=platesPurchased*platePrice;
-    subTotal=(totalCupsCost + totalPlatesCost)*(salesTaxRate);
-    totalCost=subTotal+salesTaxRate;
-    //display totalCost
-    cout<<"Total Cost is: $"<<totalCost<<endl;    
-    system("pause");
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+void calcTotal();
+int main(int argc, char** argv) {
+	calcTotal();
 	return 0;
+}
+void calcTotal(){
+	double cupPrice = 0.0;
+	int numCups = 0;
+	double platePrice = 0.0;
+	int numPlates = 0;
+	double salesTax = 0.0;
+	double subTotal = 0.0;
+	double total = 0.0;
+	double totalPlateCost = 0.0;
+	double totalCupCost = 0.0;
+	double platesPurchased = 0.0;
+	double cupsPurchased = 0.0;
+	cout << "Enter the number of cups " << endl;
+	cin  >> numCups;
+	cout << "Enter the number of plates " << endl;
+	cin >> numPlates;
+	cout << "Enter the plate price " << endl;
+	cin >> platePrice;
+	cout << "Enter the cup price " << endl;
+	cin >> cupPrice;
+	cout << "Enter the sales tax in decimal form for your province/state. For example 15% would be .15 " << endl;
+	cin >> salesTax;
+	totalPlateCost = platesPurchased * platePrice;
+	totalCupCost = cupsPurchased * cupPrice;
+	subTotal = totalCupCost + totalPlateCost;
+	total = (subTotal * salesTax) + subTotal;
+	cout << "The total cost is: " << total << "Thank you for shopping at Party On Supplies.  We appreciate your business" << endl;
+}
